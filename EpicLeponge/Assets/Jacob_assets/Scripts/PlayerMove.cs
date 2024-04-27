8fccad1a053db77d64d44024c9c932ec4c50d141
joppy2402 <joppy2402@gmail.com>
2024-04-27 16:36:31 +1200

    tests

---
+++ EpicLeponge/Assets/Jacob_assets/Scripts/PlayerMove.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log(Input.mousePosition);
+        //Vector2 mousePosition = Input.mousePosition;
+        //transform.LookAt(mousePosition);
+    }
+}
