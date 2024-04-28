fedc238639f73605af9d57d636bf9f93569ab9e8
kiwisshouldfly <117633084+kiwisshouldfly@users.noreply.github.com>
2024-04-28 18:22:41 +1200

    Added in Main Menu stuffs
    here u go

--- EpicLeponge/Assets/scripts/MainMenu.cs
+++ EpicLeponge/Assets/scripts/MainMenu.cs
@@ -1,7 +1,23 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Video;
 
-public class MainMenu : MonoBehaviour {
+public class MainMenu : MonoBehaviour 
+{
+    public VideoPlayer videoPlayer;
+    public AudioSource audioSource;
+    private bool VideoFinished = false;
+    public GameObject startButton;
+    public GameObject quitButton;
+
+    void Start()
+    {
+        Debug.Log("started");
+        videoPlayer.loopPointReached += OnVideoFinished;
+        startButton.SetActive(false);
+        quitButton.SetActive(false);
+
+    }
     public void GoToScene(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
@@ -11,5 +27,14 @@ public class MainMenu : MonoBehaviour {
         Application.Quit();
         Debug.Log("Application has been quit");
 
+    }
+
+    void OnVideoFinished(VideoPlayer source)
+    {
+        Debug.Log("finished");
+        audioSource.Play();
+        VideoFinished = true;
+        startButton.SetActive(true);
+        quitButton.SetActive(true);
     }
 }
