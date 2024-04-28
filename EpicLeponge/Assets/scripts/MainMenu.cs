642d4d57299b092e6e33a322508ebe57dcac72b5
joppy2402 <joppy2402@gmail.com>
2024-04-28 20:10:20 +1200

    controls menu finished

--- EpicLeponge/Assets/scripts/MainMenu.cs
+++ EpicLeponge/Assets/scripts/MainMenu.cs
@@ -4,39 +4,24 @@ using UnityEngine.Video;
 
 public class MainMenu : MonoBehaviour 
 {
-    public VideoPlayer videoPlayer;
-    public AudioSource audioSource;
-    private bool VideoFinished = false;
+ 
     public GameObject startButton;
-    public GameObject quitButton;
-    public GameObject options;
+    
 
     void Start()
     {
-        Debug.Log("started");
-        videoPlayer.loopPointReached += OnVideoFinished;
-        startButton.SetActive(false);
-        quitButton.SetActive(false);
-        options.SetActive(false);
+      
     }
     public void GoToScene(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
     }
-    public void QuitApp()
-    {
+    /*public void QuitApp()
+    //{
         Application.Quit();
         Debug.Log("Application has been quit");
 
-    }
+    }*/
 
-    void OnVideoFinished(VideoPlayer source)
-    {
-        Debug.Log("finished");
-        audioSource.Play();
-        VideoFinished = true;
-        startButton.SetActive(true);
-        quitButton.SetActive(true);
-        options.SetActive(true);
-    }
+    
 }
