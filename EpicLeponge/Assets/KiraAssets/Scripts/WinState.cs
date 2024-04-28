73d1ff898f54ccbff2cf5b52c27bf1302a311d89
kiwisshouldfly <117633084+kiwisshouldfly@users.noreply.github.com>
2024-04-28 21:01:41 +1200

    set up end scene

--- EpicLeponge/Assets/KiraAssets/Scripts/WinState.cs
+++ EpicLeponge/Assets/KiraAssets/Scripts/WinState.cs
@@ -1,11 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class WinState : MonoBehaviour
 {
-    public GameObject[] allGameObjects; 
-
+    public GameObject[] allGameObjects;
+    public bool wongame == false;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,6 +21,7 @@ public class WinState : MonoBehaviour
             if (obj.GetComponent<Shatterable>().hasCleaned == true)
             {
                 Debug.Log("youwon!");
+                SceneManager.LoadScene("EndScene");
             }
         }
         
