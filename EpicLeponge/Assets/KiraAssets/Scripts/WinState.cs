d6197d8810f0d0bdff733621d34331af419bd92f
kiwisshouldfly <117633084+kiwisshouldfly@users.noreply.github.com>
2024-04-28 21:40:36 +1200

    win game

--- EpicLeponge/Assets/KiraAssets/Scripts/WinState.cs
+++ EpicLeponge/Assets/KiraAssets/Scripts/WinState.cs
@@ -6,7 +6,7 @@ using UnityEngine.SceneManagement;
 public class WinState : MonoBehaviour
 {
     public GameObject[] allGameObjects;
-    public bool wongame == false;
+    //public bool wongame = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,14 +16,44 @@ public class WinState : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        foreach(GameObject obj in allGameObjects)
+        if (Input.GetKeyDown(KeyCode.Escape))
         {
-            if (obj.GetComponent<Shatterable>().hasCleaned == true)
-            {
-                Debug.Log("youwon!");
-                SceneManager.LoadScene("EndScene");
-            }
+            Debug.Log("pls");
+            DONEplease();
         }
+        //private bool isWon()
+        {
+            int clean = 0;
+            for (int i = 0; i < allGameObjects.Length; i++)
+            {
+                if (allGameObjects[i].GetComponent<Shatterable>().hasCleaned)
+                {
+                    clean++;
+                    Debug.Log(clean + " have been cleaned out of " + allGameObjects.Length);
+                }
+                if (clean == allGameObjects.Length - 1)
+                {
+                    Debug.Log("You won!");
+                    DONEplease();
+                }
+            }
+            
+            
+        }
+       
+        void DONEplease()
+        {
+            Application.Quit();
+        }
+        //bool allCleaned = true;
+        //foreach(GameObject obj in allGameObjects)
+        //{
+        //    if (obj.GetComponent<Shatterable>().hasCleaned == true)
+         //   {
+         //       Debug.Log("youwon!");
+                //SceneManager.LoadScene("EndScene");
+        //    }
+        
         
     }
 }
